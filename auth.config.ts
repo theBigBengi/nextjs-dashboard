cd3bfb5363f7a71c787456abc1fafb5476995968
theBigBengi <imthebigbengi@gmail.com>
2023-11-18 01:41:20 +0200

    add auth with next auth

---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    // The authorized callback is used to verify if the request is authorized to access a page via Next.js Middleware.
+    //  It is called before a request is completed, and it receives an object with the auth and request properties.
+    //  The auth property contains the user's session, and the request property contains the incoming request.
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // an array where you list different login options
+} satisfies NextAuthConfig;
